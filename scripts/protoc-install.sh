ad43699bface1234ecca4e9a85c2b32910011cdf
Alex Kalyvitis <alex.kalyvitis@gmail.com>
2020-04-02 12:22:48 +0200

    change protoc install

--- scripts/protoc-install.sh
+++ scripts/protoc-install.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-GO111MODULE=off
-
-go get -u \
+GO111MODULE=off go get -u \
     github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway \
     github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger \
     github.com/golang/protobuf/protoc-gen-go
